ea73438b23fbbc9064d102747453f2c5d0a4b7b4
noahssarcastic <niniguezg@gmail.com>
2021-12-01 09:50:46 -0600

    Clean up and format files

--- node-eslint/.eslintrc.js
+++ node-eslint/.eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
